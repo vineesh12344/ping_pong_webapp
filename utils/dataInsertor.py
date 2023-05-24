5c2eac08d3a5c08b1e4c3f1b809878760d2247f8
Nimmagadda Vineesh <vineesh@modelfactory.local>
2023-05-24 17:52:30 +0800

    made .env file

--- utils/dataInsertor.py
+++ utils/dataInsertor.py
@@ -1,5 +1,3 @@
-API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltYXNkd3VnaG1haHJzY2d0aGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ5MjEyMzQsImV4cCI6MjAwMDQ5NzIzNH0.0WB0xaC1iKRrvzNGpafCW3xL844VwLnpMNdGMh2x5cE"
-
 
 class dataInsertor:
     """
