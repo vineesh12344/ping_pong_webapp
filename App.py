e16e0400da23fe51b20eb8355107dbd4d64a2ea4
Nimmagadda Vineesh <vineesh@modelfactory.local>
2023-05-25 13:10:35 +0800

    finished the thing

--- App.py
+++ App.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 from utils.FirebaseUtils import FirebaseInsertor
 
+@st.cache_resource
+def initFirebase():
+    inserter = FirebaseInsertor()
+
+    return inserter
+inserter = initFirebase()
+
 st.title("🏓 Ping Pong Tracker")
 
 # Set-up players 
@@ -16,7 +23,6 @@ with n2:
 if 'vin_score' not in st.session_state:
     st.session_state.vin_score = 0
     
-
 if 'ka_score' not in st.session_state:
     st.session_state.ka_score = 0
 
@@ -24,36 +30,24 @@ if 'set_score_list' not in st.session_state:
     st.session_state.set_score_list = []
 
 if "gameData" not in st.session_state:
-    st.session_state.gameData = {
-        1: st.session_state.set_score_list,
-    }
+    st.session_state.gameData = {}
 
 player1_button = st.sidebar.button(PLAYER_1, use_container_width = True)
 player2_button = st.sidebar.button(PLAYER_2, use_container_width = True)
 
-def updateSetPointTracker(set_score_list):
-    return st.table({'Points':np.arange(1,len(st.session_state.set_score_list)+1),'Player Scored':st.session_state.set_score_list})
-def updateGamePointTracker(gameData):
-    return st.table({'Set':gameData.keys(),'Points':gameData.values()})
-
-# Display total score
-st.table({'Person' : ["Vineesh","Kaleb"],"Score":[st.session_state.vin_score,st.session_state.ka_score]})
 
 if player1_button:
     st.session_state.vin_score += 1
     st.session_state.set_score_list.append(PLAYER_1)
-
 elif player2_button:
     st.session_state.ka_score += 1
     st.session_state.set_score_list.append(PLAYER_2)
 
 # Extract current set from gameData
-set_No = max(st.session_state.gameData.keys())
-# Game tracker
-st.markdown(f"## 🎯 Set {set_No} Point Tracker")
-with st.expander(f"🧾 Points Tracker..."):
-    setPointTracker = updateSetPointTracker(st.session_state.set_score_list)
-
+try:
+    set_No = max(st.session_state.gameData.keys())
+except:
+    set_No = 0
 
 c1, c2,c3 = st.columns([1, 1,1])
 
@@ -66,25 +60,48 @@ with c2:
 with c3:
     end_game = st.button("End Game")
 
-# display gameData
-st.markdown(f"## 🧾 Total Game Data")
-with st.expander(f" 🧾 Game Data..."):
-    gamePointTracker = updateGamePointTracker(st.session_state.gameData)
-
-if reset:
+def resetPage():
     del st.session_state.vin_score
     del st.session_state.ka_score
     del st.session_state.set_score_list
+    del st.session_state.gameData
     st.experimental_rerun()
+
+if reset:
+    resetPage()
 
 # Logic to end set
 if end_set:
-    # Make a copy of gameData
-    gameData = st.session_state.gameData.copy()
+    print(st.session_state.gameData)
+    last_score_copy = st.session_state.set_score_list.copy()
     # Reset set_score_list 
     st.session_state.set_score_list = []
     # Add new set key to gameData
-    gameData[set_No+1] = st.session_state.set_score_list
+    st.session_state.gameData[set_No+1] = dict(enumerate(last_score_copy))
+    st.session_state.vin_score = 0
+    st.session_state.ka_score = 0
+
+# Display total score
+st.table({'Person' : ["Vineesh","Kaleb"],"Score":[st.session_state.vin_score,st.session_state.ka_score]})
+
+for set in st.session_state.gameData.keys():
+    st.markdown(f"## 🎯 Set {set} Point Tracker")
+    with st.container():
+        with st.expander("Points tracker.."):
+            st.table(st.session_state.gameData[set])
+
+
 
 if end_game:
-    pass
+    output = {}
+    for set in st.session_state.gameData.keys():
+        output[set] = list(st.session_state.gameData[set].values())
+
+    # Display game data
+    st.markdown(f"## 🎯 Game Tracker")
+    with st.expander("Game tracker.."):
+        st.table({'sets' : output.keys(),'winners': output.values()})
+
+    gameId = inserter.addGameData(output)
+
+    st.info(f'Game with id {gameId} inserted into your ass', icon="ℹ️")
